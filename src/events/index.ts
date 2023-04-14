e4797fa6658b5fc6e080c6df3d0215d0df3ca623
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-04-14 17:25:21 +0900

    :zap: Loggerのリスナーを追加

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,7 +2,20 @@ import { Base } from '../Base';
 import { ready } from './ready';
 
 export const subscribeEvents = (base: Base) => {
+    // discord events
     base.discord.on('ready', async () => ready(base));
 
+    // logger events
+    base.logger.on('system', (message) => {
+        if (process.argv.includes('--debug')) {
+            console.log(message);
+        }
+    });
+
+    base.logger.on('system', (message) => console.log(message));
+
+    base.logger.on('info', (message) => console.log(message));
+
+    // client login
     base.discord.login(base.discordToken);
 };
